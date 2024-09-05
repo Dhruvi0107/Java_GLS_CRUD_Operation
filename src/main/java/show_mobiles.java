2a6d1150f22ebc1543ec8f5306e4d4f64735fe08
Dhruvi0107 <132803243+Dhruvi0107@users.noreply.github.com>
2024-09-05 15:11:18 +0530

    ALL CRUD OPERATION

--- src/main/java/show_mobiles.java
+++ src/main/java/show_mobiles.java
@@ -39,7 +39,7 @@ public class show_mobiles extends HttpServlet {
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_clg","root","Deep@1904");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_clg","root","Dhruvi@0107");
 			PreparedStatement pst = con.prepareStatement("select * from Mobiles;");
 			
 			ResultSet rs = pst.executeQuery();
